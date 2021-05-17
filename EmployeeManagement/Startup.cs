eaa22035094774e203969ca86f47a051265485f5
Anand Gallla <gallaanand24@gmail.com>
2021-05-17 22:27:08 +0530

    Understood what are configuration sources in ASP.NET Core

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,6 +13,12 @@ namespace EmployeeManagement
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,9 +37,9 @@ namespace EmployeeManagement
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapGet("/", async context => 
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(_config["MyKey"]);
                 });
             });
         }
